1d0e5fd1c217474153af75cefd82d2545bc611ec
Taku Nagashima <taku.nagashima4112@gmail.com>
2017-08-31 06:51:45 +0000

    add : php_practice file

---
+++ php_practice.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<!-- by HTML5 -->
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<!-- for resposive-design meta-tag-->
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>php-test</title>
+	<link href="images/favicon.ico" rel="shortcut icon">
+	
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/php_practice.css">
+	<!-- javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<!-- anytools-->
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900|Raleway:400,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+	<?php 
+		echo "conithiwa";
+	?>
+
+</body>
+</html>
